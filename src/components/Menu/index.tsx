b707d946b5e04d50d968203422c48a3523700141
leoreisdias <leoanrdord99@gmail.com>
2020-09-30 12:11:58 -0300

    menu under card add

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
+import { QRCode } from 'react-native-qrcode-svg';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import QRCode from 'react-native-qrcode'
 
-import { Container } from './styles';
+import { Container, Code, Nav, NavItem, NavText, SignOutButton, SignOutButtonText } from './styles';
 
 const Menu: React.FC = () => {
     return (
         <Container>
+            <Code>
+                <QRCode
+                    value="https://www.linkedin.com/in/leonardord99/"
+                    backgroundColor='white'
+                    color="#8b10ae"
+                />
+            </Code>
 
+            <Nav>
+                <NavItem>
+                    <Icon name="help-outline" size={20} color="#fff" />
+                    <NavText>Me ajuda</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="person-outline" size={20} color="#fff" />
+                    <NavText>Perfil</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="credit-card" size={20} color="#fff" />
+                    <NavText>Configurar cartão</NavText>
+                </NavItem>
+                <NavItem>
+                    <Icon name="smartphone" size={20} color="#fff" />
+                    <NavText>Configurações do App</NavText>
+                </NavItem>
+            </Nav>
+
+            <SignOutButton onPress={() => { }}>
+                <SignOutButtonText>Sair do APP</SignOutButtonText>
+            </SignOutButton>
         </Container>
     );
 }
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
+import { Animated } from 'react-native'
+import { PanGestureHandler, State } from 'react-native-gesture-handler'
+
 import Header from '../../components/Header'
 import Tabs from '../../components/Tabs'
 import Menu from '../../components/Menu'
@@ -8,6 +11,25 @@ import Menu from '../../components/Menu'
 import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 const Main: React.FC = () => {
+    const translateY = new Animated.Value(0);
+
+    const animatedEvent = Animated.event(
+        [
+            {
+                nativeEvent: {
+                    translationY: translateY,
+                }
+            }
+        ],
+        {
+            useNativeDriver: true
+        }
+    )
+
+    function onHandlerStateChanged(event: any) {
+
+    }
+
     return (
         <Container>
             <Header />
@@ -15,21 +37,27 @@ const Main: React.FC = () => {
             <Content>
                 <Menu />
 
-                <Card>
-                    <CardHeader>
-                        <Icon name="attach-money" size={28} color="#666" />
-                        <Icon name="visibility-off" size={28} color="#666" />
-                    </CardHeader>
-                    <CardContent>
-                        <Title>Saldo Disponível</Title>
-                        <Description>R$ 200.000,01</Description>
-                    </CardContent>
-                    <CardFooter>
-                        <Annotation>
-                            Pagamento de Amazon.prime no valor de R$ 9,99 hoje às 09:00h.
+                <PanGestureHandler
+                    onGestureEvent={animatedEvent}
+                    onHandlerStateChange={onHandlerStateChanged}
+                >
+                    <Card>
+                        <CardHeader>
+                            <Icon name="attach-money" size={28} color="#666" />
+                            <Icon name="visibility-off" size={28} color="#666" />
+                        </CardHeader>
+                        <CardContent>
+                            <Title>Saldo Disponível</Title>
+                            <Description>R$ 200.000,01</Description>
+                        </CardContent>
+                        <CardFooter>
+                            <Annotation>
+                                Pagamento de Amazon.prime no valor de R$ 9,99 hoje às 09:00h.
                         </Annotation>
-                    </CardFooter>
-                </Card>
+                        </CardFooter>
+                    </Card>
+                </PanGestureHandler>
+
             </Content>
 
             <Tabs />
