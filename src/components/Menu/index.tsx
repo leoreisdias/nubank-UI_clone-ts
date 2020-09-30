151697033b910eef331368ff76acbec6cd0c727f
leoreisdias <leoanrdord99@gmail.com>
2020-09-30 11:17:53 -0300

    cash information on center

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import QRCode from 'react-native-qrcode'
+
+import { Container } from './styles';
+
+const Menu: React.FC = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+}
+
+export default Menu;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Header from '../../components/Header'
 import Tabs from '../../components/Tabs'
+import Menu from '../../components/Menu'
 
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 const Main: React.FC = () => {
     return (
         <Container>
             <Header />
+
+            <Content>
+                <Menu />
+
+                <Card>
+                    <CardHeader>
+                        <Icon name="attach-money" size={28} color="#666" />
+                        <Icon name="visibility-off" size={28} color="#666" />
+                    </CardHeader>
+                    <CardContent>
+                        <Title>Saldo Disponível</Title>
+                        <Description>R$ 200.000,01</Description>
+                    </CardContent>
+                    <CardFooter>
+                        <Annotation>
+                            Pagamento de Amazon.prime no valor de R$ 9,99 hoje às 09:00h.
+                        </Annotation>
+                    </CardFooter>
+                </Card>
+            </Content>
+
             <Tabs />
         </Container>
     )
